52de8ab085329ce0f514c6a5af3a5ddaf44bf871
Wen Kokke <wenkokke@users.noreply.github.com>
2023-07-24 22:56:39 +0100

    Improved navbar

--- script/index.js
+++ script/index.js
@@ -18,4 +18,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  // Get all "navbar-dropdown" elements
+  const $navbarDropdowns = Array.prototype.slice.call(document.querySelectorAll('.navbar-dropdown'), 0);
+
+  // Add a click event on each of them
+  $navbarDropdowns.forEach(el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
 });
