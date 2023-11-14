6cb5413d0e189e599fa3004fd924a356f03f9acc
Wen Kokke <wenkokke@users.noreply.github.com>
2023-11-14 23:05:27 +0000

    Inline App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Page from "./Page";
 
 const root = createRoot(document.getElementById("root")!);
+
+const items = [
+  { title: "Home", url: "/" },
+  { title: "About", url: "/about" },
+];
+
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <div className="container is-max-widescreen">
+        <Page title="keri.is" items={items}>
+          <p>hello</p>
+        </Page>
+      </div>
     </HelmetProvider>
   </React.StrictMode>
 );
