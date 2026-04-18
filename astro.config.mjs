00920e8327d18384e9f765575937a07eb7b4ae1e
Wen Kokke <wenkokke@users.noreply.github.com>
2026-04-18 15:37:33 +0100

    fuck(everything): and restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,71 +1,51 @@
 // @ts-check
-/**
- * @typedef {import("mdast-util-to-hast").State} State
- * @typedef {import("mdast").Node} MdastNode
- * @typedef {import("@benrbray/mdast-util-cite").InlineCiteNode} MdastCite
- * @typedef {import("hast").Node} HastNode
- * @typedef {import("hast").Text} HastText
- */
-import { defineConfig } from "astro/config";
-import remarkBehead from "remark-behead";
-import remarkBracketedSpans2 from "remark-bracketed-spans-2";
-import remarkCite from "./src/plugins/remark-cite.ts";
-import remarkCustomHeaderId from "remark-custom-header-id";
-import remarkDirective from "remark-directive";
-import remarkMath from "remark-math";
-import remarkSmartyPants from "remark-smartypants";
-import rehypeMathJax from "rehype-mathjax";
-import rehypeSlug from "rehype-slug";
-import { bracketedSpanToHast } from "mdast-util-bracketed-spans";
-import remarkRehypeCite, {
-  loadBibTeX,
-} from "./src/plugins/remarkRehype-cite.ts";
-import remarkTufte from "./src/plugins/remark-tufte.ts";
-import remarkRehypeTufte from "./src/plugins/remarkRehype-tufte.ts";
-import rehypeTufte from "./src/plugins/rehype-tufte.ts";
-import rehypeHeadingAnchor from "./src/plugins/rehype-heading-anchor.ts";
-
-// MathJax options:
-const MathJax = {
-  // TeX Input Processor Options
-  // https://docs.mathjax.org/en/latest/options/input/tex.html
-  tex: {},
-};
-
-// Citation options:
-const bibliography = await loadBibTeX("./src/assets/bibliography.bib");
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://keri.is",
-  base: import.meta.env.DEV ? "" : "",
-  markdown: {
-    syntaxHighlight: "prism",
-    remarkPlugins: [
-      [remarkBehead, { depth: 1 }],
-      // @ts-ignore
-      remarkBracketedSpans2,
-      remarkCite,
-      remarkCustomHeaderId,
-      remarkDirective,
-      remarkMath,
-      // @ts-ignore
-      [remarkSmartyPants, { dashes: "oldschool" }],
-      remarkTufte,
-    ],
-    remarkRehype: {
-      handlers: {
-        bracketedSpan: bracketedSpanToHast,
-        ...remarkRehypeCite({ bibliography }),
-        ...remarkRehypeTufte(),
-      },
-    },
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeHeadingAnchor,
-      [rehypeMathJax, MathJax],
-      rehypeTufte,
-    ],
-    gfm: true,
-  },
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Brownist",
+        cssVariable: "--font-brownist",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/Brownist-Regular.woff2'],
+                weight: 'normal',
+                style: 'normal',
+            }]
+        }
+    },{
+        provider: fontProviders.local(),
+        name: "Bubble Boop",
+        cssVariable: "--font-bubble-boop",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/BubbleBoop-Regular.woff2'],
+                weight: 'normal',
+                style: 'normal',
+            }]
+        }
+    },{
+        provider: fontProviders.local(),
+        name: "Jojoba",
+        cssVariable: "--font-jojoba",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/Jojoba-Regular.woff2'],
+                weight: 'normal',
+                style: 'normal',
+            }]
+        }
+    },{
+        provider: fontProviders.local(),
+        name: "Pencil Child",
+        cssVariable: "--font-pencil-child",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/PencilChild-Regular.woff2'],
+                weight: 'normal',
+                style: 'normal',
+            }]
+        }
+    }]
 });
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const siteTitle = "keri.is";
